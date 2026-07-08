eaee06289902476bfbbf6aa3a6ec661e3c1bca1e
Viszket Attila <viszket.attila1995@gmail.com>
2026-07-08 09:08:00 +0200

    first arrays in score

---
+++ arrays/scores.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+float average(int length, int numbers[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum / (float) length;
+}
+
+int main(void)
+{
+    const int N = 3;
+    int scores[N];
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+    
+    printf("Average: %f\n", average(N, scores));
+}
